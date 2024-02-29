6250494f821bc8571d46a1775e8c0816f5330329
danieldotcomcoder <daniel_shdeed@hotmail.com>
2024-02-29 17:42:21 +0200

    Implement Knn classification

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,23 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import Comp from './components/Comp.jsx';
+import Knearestneighbor from './components/Knearestneighbor.jsx';
+import './index.css';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+
+const router = createBrowserRouter([
+  {
+    path: '/React-2d',
+    element: <Comp />,
+  },
+  {
+    path: '/React-2d/Knn',
+    element: <Knearestneighbor />,
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
